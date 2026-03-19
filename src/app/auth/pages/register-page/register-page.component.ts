49da1d16167d7a2aac5a8fb17d64f0f9e0dc95b8
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-03-19 14:56:00 -0600

    register-page Form: Validaciones personalizadas en los campos name y email añadidas

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { cantBeStrider } from '../../../shared/validators/validators';
+import * as CustomValidators from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -12,9 +12,18 @@ export class RegisterPageComponent {
   constructor(private fb: FormBuilder) {}
 
   public myForm: FormGroup = this.fb.group({
-    name: ["", [ Validators.required ]],
-    email: ["", [ Validators.required, ] ],
-    username: ["", [ Validators.required, cantBeStrider ] ],
+    name: ["", [
+      Validators.required,
+      Validators.pattern(CustomValidators.firstNameAndLastnamePattern)
+
+    ]],
+    //email: ["", [ Validators.required, Validators.email] ],
+    email: ["", [
+      Validators.required,
+      Validators.pattern(CustomValidators.emailPattern),
+
+    ] ],
+    username: ["", [ Validators.required, CustomValidators.cantBeStrider ] ],
     password: ["", [ Validators.required, Validators.minLength(6)] ],
     password2: ["", [ Validators.required, ] ],
   });
--- src/app/shared/validators/validators.ts
+++ src/app/shared/validators/validators.ts
@@ -1,5 +1,10 @@
 import { FormControl, ValidationErrors } from "@angular/forms";
 
+// Validaciones del campo name
+export const firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+// Validaciones del campo email
+export const emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
 export const cantBeStrider = (control: FormControl): ValidationErrors | null => {
 
